ddad5305f700ab53e176ab63ae73fdf9805def40
d11wtq <chris@w3style.co.uk>
2014-02-18 00:56:02 +1100

    Simplify tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# test suite is just a dependency
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-$DIR/../needy.sh example
+cd "$(dirname "${BASH_SOURCE[0]}")"
+../needy.sh example
