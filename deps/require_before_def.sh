7f70f6728053b1a22287ca7098cc4e0ded84581c
d11wtq <chris@w3style.co.uk>
2014-02-24 00:43:16 +1100

    First iteration of defered dependency resolution (can't handle whitespace in parameterized deps)

--- deps/require_before_def.sh
+++ deps/require_before_def.sh
@@ -1,7 +1,31 @@
 require_before_def() {
-  require inner
+  require inner_1
+  require inner_2
 
-  inner() {
-    true
+  filename_1=./deps/tests/tmp/require_before_def_1.txt
+  filename_2=./deps/tests/tmp/require_before_def_2.txt
+
+  inner_1() {
+    is_met() {
+      ls $filename_1
+    }
+
+    meet() {
+      touch $filename_1
+    }
+  }
+
+  inner_2() {
+    is_met() {
+      ls $filename_2
+    }
+
+    meet() {
+      touch $filename_2
+    }
+  }
+
+  is_met() {
+    ls $filename_1 && ls $filename_2
   }
 }
