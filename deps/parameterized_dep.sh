37f10fc993b5a670e081f34ac3a277adbf08ac40
d11wtq <chris@w3style.co.uk>
2014-02-26 23:15:09 +1100

    Fix issue with quotes in dep arguments

--- deps/parameterized_dep.sh
+++ deps/parameterized_dep.sh
@@ -1,13 +1,13 @@
 parameterized_dep() {
   outer_filename=./deps/tests/tmp/parameterized_dep.txt
-  outer_contents="one two three"
+  outer_contents="one \"two\\ three"
 
   write_file() {
     filename=$1
     contents=$2
 
     is_met() {
-      [[ `cat $filename` = $contents ]]
+      [[ `cat $filename` = "$contents" ]]
     }
 
     meet() {
@@ -16,7 +16,7 @@ parameterized_dep() {
   }
 
   is_met() {
-    [[ `cat $outer_filename` = $outer_contents ]]
+    [[ `cat $outer_filename` = "$outer_contents" ]]
   }
 
   require write_file $outer_filename "$outer_contents"
